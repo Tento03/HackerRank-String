a765d22a0bc968fa36540429beac3a87440f1a95
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-11-01 11:51:45 +0700

    camel case

---
+++ Easy/CamelCase.kt
@@ -0,0 +1,44 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+fun camelcase(s: String): Int {
+    // Write your code here
+    var total=0
+    for (c in s){
+        if(c.isUpperCase()){
+            total++
+        }
+    }
+    return total+1
+}
+
+fun main(args: Array<String>) {
+    val s = readLine()!!
+
+    val result = camelcase(s)
+
+    println(result)
+}
